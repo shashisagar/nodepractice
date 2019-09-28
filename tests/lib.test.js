e02f24a98711db3f9f581f6a4db1c8b2b9d0a7d5
shashi sagar <shashi.sagar@bacancytechnology.com>
2019-09-28 07:21:29 +0530

    test check from vc

--- tests/lib.test.js
+++ tests/lib.test.js
@@ -1,6 +1,6 @@
 const sum = require('./lib');
 
 test('adds 1 + 2 to equal 3', () => {
-  const result=sum.absolute(0);  
-  expect(sum(1, 2)).toBe(3);
+  const result1=sum.absolute(0);  
+  expect(sum(1, 2)).toBe(4);
 });
